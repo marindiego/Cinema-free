9aa6db7460864c96990e3ab8b454af7985b486c0
Diego Marin <paolo05061@gmail.com>
2024-04-17 11:51:12 -0300

    Homework 6 asincronismo

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,12 +1,16 @@
 
 const APIFilms = "https://students-api.up.railway.app/movies";
 const renderFilmsCards = require("./renderFilmsCards");
+const axios = require("axios");
 
-const getFilms = () => {
-    $.get(APIFilms,(data,status)=> {
-        if(status !== "success") return alert("Not films found");
-        renderFilmsCards(data);
-    })
+const fetchFilms = async () => {
+    try {
+        const response = await axios.get(APIFilms);
+        const films = response.data;
+        renderFilmsCards(films);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-getFilms();
+fetchFilms();
