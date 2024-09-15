dcf8a635a9bf8bf3d3d6aa47c86187842295cd2d
Diego Marin <paolo05061@gmail.com>
2024-09-15 14:11:37 -0300

    adding PORT variable

--- back/index.js
+++ back/index.js
@@ -1,9 +1,10 @@
 // Import the Express application from the './src/server' file and assign it to the 'app' variable.
 const app = require("./src/server");
 const connectDB = require("./src/config/conDb");
+const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
-    app.listen(3000, "0.0.0.0", () => {
+    app.listen(PORT, "0.0.0.0", () => {
         console.log("Server is running on port 3000");
     });
 })
